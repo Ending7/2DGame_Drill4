2204797e84de79b9a6fc9077c253509375bf724e
Jin Young Jang - MyPC <ending1025@gmail.com>
2023-09-25 22:46:45 +0900

    Make key events
    esc(quit), character move u,r,d,l

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -6,16 +6,52 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
 player = load_image('player.png')
 
+running = True
+x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+dirX, dirY = 0, 0
 
-
+def handle_events():
+    global running, dirX, dirY
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE: 
+                running = False
+            elif event.key == SDLK_LEFT:
+                dirX -=1
+            elif event.key == SDLK_RIGHT:
+                dirX +=1
+            elif event.key == SDLK_UP:
+                dirY +=1
+            elif event.key == SDLK_DOWN:
+                dirY -=1
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_LEFT:
+                dirX +=1
+            elif event.key == SDLK_RIGHT:
+                dirX -=1
+            elif event.key == SDLK_UP:
+                dirY -=1
+            elif event.key == SDLK_DOWN:
+                dirY +=1
 
 def rendering():
-    while True:
+    global dirX, dirY, x, y
+    while running:
         clear_canvas()
         tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-        player.clip_draw(32,683-72,38,45,50,100,100,100)
+        player.clip_draw(32,683-72,38,45,x,y, 100,100)
         update_canvas()
+        handle_events()
 
+        x+= dirX * 10
+        y+= dirY * 10
+        delay(0.05)
+
+        if not running:
+            break
 
 
 
