b2ec50fbc71b8c926c211cbf72fffb41d3ca8f41
Jin Young Jang - MyPC <ending1025@gmail.com>
2023-09-26 01:49:09 +0900

    Animation Attack

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -13,14 +13,14 @@ dirX, dirY, motion, tempmotion, index = 0, 0, 0, 3, 0
 def again(frame):
     global index, x, y  
     frameX, frameY, width, height = frame[index]
-    if motion == 0 or motion == 3 or motion == 5 or motion == 7:
+    if motion == 0 or motion == 3 or motion == 5 or motion == 7 or motion == 9:
         player.clip_draw(frameX,frameY,width,height,x,y, 100,100)
-    elif motion == 1 or motion == 2 or motion == 4 or motion == 6:
+    elif motion == 1 or motion == 2 or motion == 4 or motion == 6 or motion == 8:
         player.clip_composite_draw(frameX,frameY,width,height, 0, 'h', x, y, 100,100)
 
 def player_idle():
     global index  
-    frame = [(32, 683-72,39,45), (83, 683-72,39,45), (132, 683-72,39,45), (182, 683-72,39,45)]
+    frame = [(32, 683-72,39,60), (83, 683-72,39,60), (132, 683-72,39,60), (182, 683-72,39,60)]
     again(frame) 
     index += 1
     if(index == 4):
@@ -28,18 +28,27 @@ def player_idle():
 
 def player_die():
     global index
-    frame = [(32, 683-203,35,50), (70, 683-203,40,50), (120,683-203,57,50), (180,683-203,43,50), (227,683-203,43,50)]
+    frame = [(32, 683-203,35,60), (70, 683-203,40,60), (120,683-203,57,60), (180,683-203,43,60), (227,683-203,43,60)]
     again(frame) 
     index += 1
     if(index == 5):
         index = 0
+
 def player_attack():
-    pass
+    global index
+    frame = [(29, 683-362,37,60), (74, 683-362,51,60),(131, 683-362,46,60),(179, 683-362,45,60),(223, 683-362,47,60),
+    (273, 683-362,57,60),(330, 683-362,52,60),(386, 683-362,36,60),(435, 683-362,33,60),(472, 683-362,36,60),
+    (514, 683-362,55,60),(569, 683-362,39,60),(611, 683-362,36,60),(652, 683-362,38,60)]
+    again(frame) 
+    index += 1
+    if(index == 14):
+        index = 0
+
 def player_run():
     global index
-    frame = [(27,683-428,40,45), (71, 683-428,41,45), (123,683-428,38,45), 
-    (173,683-428,33,45), (213,683-428,46,45), (261,683-428,41,45), 
-    (312,683-428,38,45), (358,683-428,32,45)]
+    frame = [(27,683-428,40,60), (71, 683-428,41,60), (123,683-428,38,60), 
+    (173,683-428,33,60), (213,683-428,46,60), (261,683-428,41,60), 
+    (312,683-428,38,60), (358,683-428,32,60)]
     again(frame)
     index += 1
     if(index == 8):
@@ -53,6 +62,8 @@ def player_animation():
         player_run()
     if motion == 6 or motion == 7:
         player_die()
+    if motion == 8 or motion == 9:
+        player_attack()
 
 def handle_events():
     global running, dirX, dirY, index, motion, tempmotion
@@ -64,32 +75,41 @@ def handle_events():
             if event.key == SDLK_ESCAPE: 
                 running = False
             elif event.key == SDLK_LEFT:
+                index = 0
                 dirX -=1
                 motion = 2
                 tempmotion = 2
             elif event.key == SDLK_RIGHT:
+                index = 0
                 dirX +=1
                 motion = 3
                 tempmotion = 3
             elif event.key == SDLK_UP:
+                index = 0
                 dirY +=1
                 if tempmotion == 2:
                     motion = 4
                 if tempmotion == 3:
                     motion = 5
             elif event.key == SDLK_DOWN:
+                index = 0
                 dirY -=1
                 if tempmotion == 2:
                     motion = 4
                 if tempmotion == 3:
                     motion = 5
             elif event.key == SDLK_d:
-
                 index = 0
                 if tempmotion == 2:
                     motion = 6
                 if tempmotion == 3:
                     motion = 7
+            elif event.key == SDLK_a:
+                index = 0
+                if tempmotion == 2:
+                    motion = 8
+                if tempmotion == 3:
+                    motion = 9    
 
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
@@ -120,7 +140,8 @@ def handle_events():
                 index = 0  
             elif event.key == SDLK_d:
                 index = 0
-
+            elif event.key == SDLK_a:
+                index = 0
 
 def rendering():
     global dirX, dirY, x, y
